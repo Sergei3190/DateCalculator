739051046ea9639c6092bb0e5d4cfb9d46feb218
Sergei3190 <sergkosya@mail.ru>
2019-12-03 14:23:53 +0300

    реализована операция DateDiff

--- DateCalculator/DateCalculator.Console/Program.cs
+++ DateCalculator/DateCalculator.Console/Program.cs
@@ -20,12 +20,27 @@ namespace DateCalculator.Console
                     System.Console.WriteLine("Please enter data: ");
                     System.Console.WriteLine(@"Operation type ""DateOperation|DateDiff"": ");
                     var ot = System.Console.ReadLine();                
-                    System.Console.WriteLine(@"Date time in format ""yyyy-MM-ddThh:mm:ss"": ");
-                    var d = System.Console.ReadLine();
-                    System.Console.WriteLine(@"Perform an operation ""<+|-><number><date part:'Y','M','D','h','m','s'>"": ");
-                    var o = System.Console.ReadLine();
 
-                    args = new string[] { "ot", ot, "d", d, "o", o };
+                    //здесь дописать ввод аргументов в зависимости от выбранной операции
+                    if (ot == "DateOperation")
+                    {
+                        System.Console.WriteLine(@"Date time in format ""yyyy-MM-ddThh:mm:ss"": ");
+                        var d = System.Console.ReadLine();
+                        System.Console.WriteLine(@"Perform an operation ""<+|-><number><date part:'Y','M','D','h','m','s'>"": ");
+                        var o = System.Console.ReadLine();
+                        args = new string[] { "ot", ot, "d", d, "o", o };
+                    }
+                    else if (ot == "DateDiff")
+                    {
+                        System.Console.WriteLine(@"First date time in format ""yyyy-MM-ddThh:mm:ss"": ");
+                        var d1 = System.Console.ReadLine();
+                        System.Console.WriteLine(@"Second date time in format ""yyyy-MM-ddThh:mm:ss"": ");
+                        var d2 = System.Console.ReadLine();
+                        System.Console.WriteLine(@"Perform an operation ""<+|->"": ");
+                        var o = System.Console.ReadLine();
+                        args = new string[] {"ot", ot, "d1", d1, "d2", d2, "o", o };
+                    }
+                   
                 }
                 var operationTypeString = args[1];
                 var operationType = (OperationType)Enum.Parse(typeof(OperationType), operationTypeString, true);
@@ -43,6 +58,47 @@ namespace DateCalculator.Console
                     //char[] format = {'-','+','Y','M','D','h','m','s'};
                     //var number1 = operation.Trim(format);
                     #endregion
+                }
+                else if (operationType == OperationType.DateDiff)
+                {                   
+                    var dateTimeString1 = args[3];
+                    var dateTime1 = DateTime.ParseExact(dateTimeString1, "yyyy-MM-ddThh:mm:ss", CultureInfo.InvariantCulture);
+
+                    var dateTimeString2 = args[5];
+                    var dateTime2 = DateTime.ParseExact(dateTimeString2, "yyyy-MM-ddThh:mm:ss", CultureInfo.InvariantCulture);
+
+                    var operation = args[7];
+                    System.Console.WriteLine($"operation: {operation}");
+
+                    var timeSpanDateTime1 = new TimeSpan(dateTime1.Hour, dateTime1.Minute, dateTime1.Second);
+                    var dateTime1Date = dateTime1.Subtract(timeSpanDateTime1);
+
+                    var timeSpanDateTime2 = new TimeSpan(dateTime2.Hour, dateTime2.Minute, dateTime2.Second);
+                    var dateTime2Date = dateTime2.Subtract(timeSpanDateTime2);
+
+                    DateTime dateTimeNew = new DateTime();
+
+                    if (operation == "+")
+                    {
+                        var timeSpanNew = timeSpanDateTime1 + timeSpanDateTime2;
+                        var dateTimeNewDate = dateTime1Date.AddYears(dateTime2Date.Year).AddMonths(dateTime2Date.Month).AddDays(dateTime2Date.Day);
+                        dateTimeNew = dateTimeNewDate.Add(timeSpanNew);                      
+                    }
+                    else if (operation == "-")
+                    {
+                        var timeSpanNew = timeSpanDateTime1 - timeSpanDateTime2;
+                        var number = (dateTime2Date.Year, dateTime2Date.Month, dateTime2Date.Day);
+                        number.Year *= -1;
+                        number.Month *= -1;
+                        number.Day *= -1;
+                        var dateTimeNewDate = dateTime1Date.AddYears(number.Year).AddMonths(number.Month).AddDays(number.Day);
+                        dateTimeNew = dateTimeNewDate.Add(timeSpanNew);
+                    }
+
+                    var dateDiff = dateTime1.Subtract(dateTime2);
+
+                    System.Console.WriteLine($"Result: {dateTimeNew}, dateDiff = {dateDiff}");
+
                 }
 
             } 
