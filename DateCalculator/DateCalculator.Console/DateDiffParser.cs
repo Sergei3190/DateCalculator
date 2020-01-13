7517c3b2f55a13752df3a2e516245a8194976e19
Sergei3190 <sergkosya@mail.ru>
2020-01-13 12:47:26 +0300

    Revert "корректировка реализации операции DateDiff"
    This reverts commit f4fcdfe3f4ed9e36d2ffc2dba379f253442df336.

--- DateCalculator/DateCalculator.Console/DateDiffParser.cs
+++ DateCalculator/DateCalculator.Console/DateDiffParser.cs
@@ -11,6 +11,7 @@ namespace DateCalculator.Console
     {
         public DateDiffParserResult Parser(string[] args)
         {
+            //избавится от копипаста 16 и 19 строка 
             var dateTimeString1 = args[3];
             var dateTime1 = ParserExact(dateTimeString1);
             System.Console.WriteLine($"date time1: {dateTime1}");
@@ -20,9 +21,15 @@ namespace DateCalculator.Console
             System.Console.WriteLine($"date time2: {dateTime2}");
 
             var operation = args[7];
-            System.Console.WriteLine($"operation: {operation}");          
+            System.Console.WriteLine($"operation: {operation}");
 
-            return new DateDiffParserResult(dateTime1, dateTime2, operation);
+            var timeSpanDateTime1 = new TimeSpan(dateTime1.Hour, dateTime1.Minute, dateTime1.Second);
+            var dateTime1Date = dateTime1.Subtract(timeSpanDateTime1);
+
+            var timeSpanDateTime2 = new TimeSpan(dateTime2.Hour, dateTime2.Minute, dateTime2.Second);
+            var dateTime2Date = dateTime2.Subtract(timeSpanDateTime2);
+
+            return new DateDiffParserResult(dateTime1, dateTime2, operation, timeSpanDateTime1, timeSpanDateTime2, dateTime1Date, dateTime2Date);
         }
 
         private DateTime ParserExact(string date)
