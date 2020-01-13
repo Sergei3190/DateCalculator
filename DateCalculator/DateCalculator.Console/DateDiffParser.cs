3bb44ce6b0dab5839b8591a911134e198278b459
Sergei3190 <sergkosya@mail.ru>
2020-01-13 12:52:33 +0300

    Revert "Revert "корректировка реализации операции DateDiff""
    This reverts commit 7517c3b2f55a13752df3a2e516245a8194976e19.

--- DateCalculator/DateCalculator.Console/DateDiffParser.cs
+++ DateCalculator/DateCalculator.Console/DateDiffParser.cs
@@ -11,7 +11,6 @@ namespace DateCalculator.Console
     {
         public DateDiffParserResult Parser(string[] args)
         {
-            //избавится от копипаста 16 и 19 строка 
             var dateTimeString1 = args[3];
             var dateTime1 = ParserExact(dateTimeString1);
             System.Console.WriteLine($"date time1: {dateTime1}");
@@ -21,15 +20,9 @@ namespace DateCalculator.Console
             System.Console.WriteLine($"date time2: {dateTime2}");
 
             var operation = args[7];
-            System.Console.WriteLine($"operation: {operation}");
+            System.Console.WriteLine($"operation: {operation}");          
 
-            var timeSpanDateTime1 = new TimeSpan(dateTime1.Hour, dateTime1.Minute, dateTime1.Second);
-            var dateTime1Date = dateTime1.Subtract(timeSpanDateTime1);
-
-            var timeSpanDateTime2 = new TimeSpan(dateTime2.Hour, dateTime2.Minute, dateTime2.Second);
-            var dateTime2Date = dateTime2.Subtract(timeSpanDateTime2);
-
-            return new DateDiffParserResult(dateTime1, dateTime2, operation, timeSpanDateTime1, timeSpanDateTime2, dateTime1Date, dateTime2Date);
+            return new DateDiffParserResult(dateTime1, dateTime2, operation);
         }
 
         private DateTime ParserExact(string date)
--- DateCalculator/DateCalculator.Console/DateDiffParserResult.cs
+++ DateCalculator/DateCalculator.Console/DateDiffParserResult.cs
@@ -8,25 +8,16 @@ namespace DateCalculator.Console
 {
     class DateDiffParserResult
     {
-        public DateDiffParserResult(DateTime dateTime1, DateTime dateTime2, string operation, TimeSpan timeSpanDateTime1, TimeSpan timeSpanDateTime2,
-            DateTime dateTime1Date, DateTime dateTime2Date)
+        public DateDiffParserResult(DateTime dateTime1, DateTime dateTime2, string operation)
         {
             DateTime1 = dateTime1;
             DateTime2 = dateTime2;
             Operation = operation;
-            TimeSpanDateTime1 = timeSpanDateTime1;
-            TimeSpanDateTime2 = timeSpanDateTime2;
-            DateTime1Date = dateTime1Date;
-            DateTime2Date = dateTime2Date;
         }
 
         public DateTime DateTime1 { get; }
         public DateTime DateTime2 { get; }
         public string Operation { get; }
-        public TimeSpan TimeSpanDateTime1 { get; }
-        public TimeSpan TimeSpanDateTime2 { get; }
-        public DateTime DateTime1Date { get; }
-        public DateTime DateTime2Date { get; }
 
     }
 }
